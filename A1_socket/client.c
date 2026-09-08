d6b5775a9156aa6501e4ed9a66e45826fd295af9
Ethan Chamberlain <ethanchamb2@gmail.com>
2026-09-08 09:49:31 -0400

    passing all basic given tests on A1

--- A1_socket/client.c
+++ A1_socket/client.c
@@ -7,6 +7,12 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <sys/ioctl.h>
+#include <stdbool.h>
+#include <netdb.h>
+
+#define STDINREADSIZE 10
+
 
 static void usage(const char *prog) {
     fprintf(stderr, "Usage: %s <server-ip> <server-port>\n", prog);
@@ -24,13 +30,12 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "Invalid port: %s\n", argv[2]);
         return 1;
     }
+    //fprintf(stderr, "args are %s, %s, %s\n", argv[1], argv[2], argv[3]);
+
 
     // TODO: Create a TCP socket (AF_INET, SOCK_STREAM).
     // TODO: Populate sockaddr_in with server IP/port.
     // TODO: Connect to the server.
-
-    fprintf(stderr, "args are %s, %s\n", argv[1], argv[2]);
-
     // TODO: Read from stdin in a loop (read()) and send in chunks.
     // TODO: For each chunk, send the *exact bytes* you read.
     //   - Use send()/write() in a loop to handle partial sends.
@@ -38,6 +43,64 @@ int main(int argc, char *argv[]) {
     // The test harness compares server stdout byte-for-byte with stdin input.
 
     // TODO: Close the socket before exiting.
+
+        
+
+    
+
+
+    int status;
+    struct addrinfo hints;
+    struct addrinfo *res;
+    
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+
+    if ((status = getaddrinfo(argv[1], argv[2], &hints, &res)) != 0) {
+        fprintf(stderr, "gai errorL %s\n", gai_strerror(status));
+        exit(1);
+    } 
+
+    try_soc_again:
+    int soc = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+
+    if (soc == -1){
+        fprintf(stderr,"error making socket, %s\n", strerror(errno));
+
+        if (errno == EINTR) goto try_soc_again;
+        exit(1);
+    }
+
+    connect(soc, res->ai_addr, res->ai_addrlen);
+
+    int c;
+    char buff[STDINREADSIZE];
+    char *buf_ptr = buff;
+    int read_bytes = -1;
+
+    int len =0;
+    int bytes_sent =0;
+
+
+    while (read_bytes != 0){
+        read_bytes = read(STDIN_FILENO, buf_ptr, STDINREADSIZE);
+
+        while (bytes_sent < read_bytes){
+            
+            bytes_sent += send(soc, (buf_ptr + bytes_sent), (read_bytes - bytes_sent), 0);
+            //bytes_sent += write(STDOUT_FILENO, (buf_ptr + bytes_sent), (read_bytes - bytes_sent));
+
+        }
+        bytes_sent = 0;
+    }
+    //fprintf(stdout, "read bytes 0, last read is %x\n", buf_ptr);
+
+    
+    
+    //fprintf(stdout, "ending client send\n");
+    close(soc);
+    freeaddrinfo(res);
 
     return 0;
 }
--- A1_socket/server.c
+++ A1_socket/server.c
@@ -7,12 +7,19 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <stdbool.h>
+
+
+#define READFROMSOCSIZE 20
 
 static void usage(const char *prog) {
     fprintf(stderr, "Usage: %s <listen-port>\n", prog);
 }
 
 int main(int argc, char *argv[]) {
+    int errno;
     if (argc != 2) {
         usage(argv[0]);
         return 1;
@@ -24,16 +31,14 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "Invalid port: %s\n", argv[1]);
         return 1;
     }
-
-    // int listenSoc;
+   
 
     // TODO: Create a TCP listen socket (AF_INET, SOCK_STREAM).
     // TODO: Set SO_REUSEADDR on the listen socket.
     // TODO: Bind the socket to INADDR_ANY and the given port.
     // TODO: Listen with a small backlog (e.g., 5-10).
-
-    fprintf(stderr, "args are %s\n", argv[1]);
-
+   
+    
 
     // TODO: Accept clients in an infinite loop.
     //   - For each client, read in chunks until EOF.
@@ -45,5 +50,84 @@ int main(int argc, char *argv[]) {
 
     // TODO: Close the listen socket before exiting.
 
-    return 0;
+    //fprintf(stderr, "args are %s\n", argv[1]);
+
+    int status;
+    struct addrinfo hints;
+    struct addrinfo *res;
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE; 
+
+    if ((status = getaddrinfo(INADDR_ANY, argv[1], &hints, &res)) != 0) {
+        fprintf(stderr, "gai errorL %s\n", gai_strerror(status));
+        exit(1);
+    } 
+
+    try_soc_again:
+    int soc = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+
+    if (soc == -1){
+        fprintf(stderr,"error making socket, %s\n", strerror(errno));
+
+        if (errno == EINTR) goto try_soc_again;
+        exit(1);
+    }
+
+    try_bind_again:
+    int binderr = bind(soc, res->ai_addr, res->ai_addrlen);
+
+    if (binderr == -1){
+        fprintf(stderr,"error binding socket, %s\n", strerror(errno));
+
+        if (errno == EINTR) goto try_bind_again;
+        exit(1);
+    }
+
+    int yes=1;
+    setsockopt(soc, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof yes);
+
+
+    listen(soc, 10);
+
+    struct sockaddr_storage rec_addr;
+    socklen_t addr_size;
+    int rec_soc;
+    //char *test = "temp 122212222\n";
+
+
+
+    while (true){
+
+        addr_size = sizeof rec_addr;
+        rec_soc = accept(soc, (struct sockaddr *)&rec_addr, &addr_size);
+
+        char readBuff[READFROMSOCSIZE];
+        char *buff_ptr = readBuff;
+        int read = -1;
+
+        while (read != 0){
+            read = recv(rec_soc, buff_ptr, READFROMSOCSIZE, 0);
+            int wrote = 0;
+            //*(buff_ptr + read) = '\0';
+
+            //fprintf(stdout, "read %d bytes\n", read);
+            while (wrote < read){
+                char *curr_str_ptr = (buff_ptr + wrote);
+                wrote += write(STDOUT_FILENO, curr_str_ptr, (read - wrote));
+                //fprintf(stdout, "\nsize of last print is %ld\n", strlen(curr_str_ptr));
+            }
+
+        }
+        
+        //write(STDOUT_FILENO, test, strlen(test));
+
+        //fprintf(stdout, "closing connection socket\n");
+        close(rec_soc);
+    }
+
+
+    //return 0;
 }
